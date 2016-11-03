fa77f05383d63d9941f17fac74d218b12bd6cdd1
gregg <gregg@topshelfdesign.net>
2016-11-03 10:49:06 -0400

    upadting package to do its function

--- src/acf_template_loader.php
+++ src/acf_template_loader.php
@@ -4,9 +4,27 @@ namespace topshelfdesign;
 
 class acf_template_loader{
 
-  function __construct(){
-    print 'finally!!!';
-  }
+  public function __construct($id = false, $acf_field_name = 'content')
+      {
 
+        $this->id = $id;
+        $this->acf_field_name = $acf_field_name;
+
+          $fields = $sidebar ? get_field("sidebar_content", $id) : get_field("content", $id);
+
+          $fields = get_field($this->acf_field_name, $this->id);
+
+          if (!$fields) return false;
+    
+          foreach ($fields as $field):
+              if (!file_exists(locate_template("acf-flexible-content-sidebar/" . $field['acf_fc_layout'] . ".php"))) {
+                  if (is_user_logged_in())
+                      print "<h2>Sidebar File \"{$field['acf_fc_layout']}.php\" not found, please contact the website admin.</h2>";
+              } else {
+                  include(locate_template("acf-flexible-content-sidebar/" . $field['acf_fc_layout'] . ".php"));
+              }
+          endforeach;
+
+      }
 
 }
