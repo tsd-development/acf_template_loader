56b6ab13bff7835e84bdd806dbd1972ac125f022
gregg <gregg@topshelfdesign.net>
2017-06-26 13:30:48 -0400

    updated page builder

--- src/page_builder.php
+++ src/page_builder.php
@@ -4,6 +4,15 @@ namespace topshelfdesign;
 class page_builder extends acf_template_loader
 {
 
+    public function __construct($vars)
+    {
+        parent::__construct($vars);
+
+        $this->rows = get_field("builder_rows", $this->ID);
+
+
+    }
+
     public function get_output()
     {
         ob_start();
@@ -20,24 +29,24 @@ class page_builder extends acf_template_loader
 
         switch ($this->division) {
 
-            case 'quarter_left':
-                $op = [3,9];
+            case 'quarter':
+                $op = [9, 3];
                 break;
 
-            case 'third_left':
-                $op = [4,8];
+            case 'third':
+                $op = [8, 4];
                 break;
 
             case 'half':
-                $op = [6,6];
+                $op = [6, 6];
                 break;
 
-            case 'two_thirds_left':
-                $op = [8,4];
+            case 'two-thirds':
+                $op = [4, 8];
                 break;
 
-            case 'three_quarters_left':
-                $op = [9,3];
+            case 'three-quarters':
+                $op = [3, 9];
                 break;
 
             default:
@@ -53,38 +62,40 @@ class page_builder extends acf_template_loader
     public function pb_output()
     {
 
+        foreach ($this->rows as $row):
 
-        $primary = get_field("primary_content", $this->ID);
-        $sidebar = get_field("sidebar_content", $this->ID);
+            $primary = $row['primary_content'];
+            $sidebar = $row['sidebar_content'];
 
-        $this->division = get_field("column_division", $this->ID);
+            $this->division = $row['sidebar'];
 
-        $this->columns = $this->get_division();
+            $this->columns = $this->get_division();
 
-        $l_column_content = new page_builder();
-        $l_column_content->use_custom_field_group($primary['content']);
-        $l_column_op = $l_column_content->get_output();
+            $l_column_content = new page_builder();
+            $l_column_content->use_custom_field_group($primary['layouts']);
+            $l_column_op = $l_column_content->get_output();
 
-        $r_column_content = new page_builder();
-        $r_column_content->use_custom_field_group($sidebar['content']);
-        $r_column_op = $r_column_content->get_output();
+            $r_column_content = new page_builder();
+            $r_column_content->use_custom_field_group($sidebar['layouts']);
+            $r_column_op = $r_column_content->get_output();
 
 
-        $op = "
+            $op = "
         
-        <div class='row pb_builder_output'>
-            <div class='small-12 medium-{$this->columns[0]} column'>
-                {$l_column_op}
-            </div>
-            <div class='small-12 medium-{$this->columns[1]} column'>
-                {$r_column_op}
-            </div>
-        </div>
+                <div class='row pb_builder_output'>
+                    <div class='small-12 medium-{$this->columns[0]} column'>
+                        {$l_column_op}
+                    </div>
+                    <div class='small-12 medium-{$this->columns[1]} column'>
+                        {$r_column_op}
+                    </div>
+                </div>
             
         
         ";
 
-        print $op;
+            print $op;
+        endforeach;
     }
 
 }
